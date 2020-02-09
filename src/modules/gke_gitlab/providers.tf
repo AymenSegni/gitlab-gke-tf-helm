c99eb7985a60612d76e101048327d9dc881d91a6
asegni <segniaymen1@gmail.com>
2020-02-09 02:30:13 +0100

    Providers

---
+++ src/modules/gke_gitlab/providers.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "${var.project_id}"
+}
+
+provider "google-beta" {
+  project = "${var.project_id}"
+}
+
+provider "helm" {
+  service_account = "tiller"
+  install_tiller  = true
+  namespace       = "kube-system"
+
+  kubernetes {
+    host                   = "${google_container_cluster.gitlab.endpoint}"
+    client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+    client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+  }
+}
+
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = "${google_container_cluster.gitlab.endpoint}"
+  client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
+  client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+}
