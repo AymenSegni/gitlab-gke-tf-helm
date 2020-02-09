11fa6b3058d410b23b07055b0012cb010f911a19
asegni <segniaymen1@gmail.com>
2020-02-09 02:58:48 +0100

    Variables

---
+++ src/deployment/variables.tf
@@ -0,0 +1,7 @@
+variable "project_id" {
+  description = "The project ID to deploy to"
+}
+
+variable "certmanager_email" {
+  description = "Email used to retrieve SSL certificates from Let's Encrypt"
+}
