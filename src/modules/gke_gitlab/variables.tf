bfd1eab0388eb3186a91936d9cfab77163469eb9
asegni <segniaymen1@gmail.com>
2020-02-09 02:48:11 +0100

    Variabels

---
+++ src/modules/gke_gitlab/variables.tf
@@ -0,0 +1,37 @@
+variable "project_id" {
+  description = "GCP Project to deploy resources"
+}
+
+variable "domain" {
+  description = "Domain for hosting gitlab functionality (ie mydomain.com would access gitlab at gitlab.mydomain.com)"
+  default     = ""
+}
+
+variable "certmanager_email" {
+  description = "Email used to retrieve SSL certificates from Let's Encrypt"
+}
+   
+variable "gke_version" {
+  description = "Version of GKE to use for the GitLab cluster"
+  default     = "1.14"
+}
+
+variable "gitlab_db_password" {
+  description = "Password for the GitLab Postgres user"
+  default     = ""
+}
+
+variable "gitlab_address_name" {
+  description = "Name of the address to use for GitLab ingress"
+  default     = ""
+}
+
+variable "gitlab_runner_install" {
+  description = "Choose whether to install the gitlab runner in the cluster"
+  default     = true
+}
+
+variable "region" {
+  default     = "us-central1"
+  description = "GCP region to deploy resources to"
+}
