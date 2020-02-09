e0b235279db2746d64f5ed7277aab0020049e838
asegni <segniaymen1@gmail.com>
2020-02-09 02:47:18 +0100

    TF 0.12x upgrade

--- src/modules/gke_gitlab/providers.tf
+++ src/modules/gke_gitlab/providers.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project = "${var.project_id}"
+  project = var.project_id
 }
 
 provider "google-beta" {
-  project = "${var.project_id}"
+  project = var.project_id
 }
 
 provider "helm" {
@@ -12,17 +12,17 @@ provider "helm" {
   namespace       = "kube-system"
 
   kubernetes {
-    host                   = "${google_container_cluster.gitlab.endpoint}"
-    client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
-    client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+    host                   = google_container_cluster.gitlab.endpoint
+    client_certificate     = base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)
+    client_key             = base64decode(google_container_cluster.gitlab.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)
   }
 }
 
 provider "kubernetes" {
   load_config_file       = false
-  host                   = "${google_container_cluster.gitlab.endpoint}"
-  client_certificate     = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)}"
-  client_key             = "${base64decode(google_container_cluster.gitlab.master_auth.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)}"
+  host                   = google_container_cluster.gitlab.endpoint
+  client_certificate     = base64decode(google_container_cluster.gitlab.master_auth.0.client_certificate)
+  client_key             = base64decode(google_container_cluster.gitlab.master_auth.0.client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.gitlab.master_auth.0.cluster_ca_certificate)
 }
