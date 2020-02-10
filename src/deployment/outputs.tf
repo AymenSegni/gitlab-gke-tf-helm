805c5fffdc038d993d040e1cf9aa7f70879587e2
asegni <segniaymen1@gmail.com>
2020-02-10 22:57:55 +0100

    Update outputs

--- src/deployment/outputs.tf
+++ src/deployment/outputs.tf
@@ -1,7 +1,7 @@
 output "gitlab_url" {
-  value = module.gitlab_gke.gitlab_url
+  value = module.gitlab.gitlab_url
 }
 
 output "root_password_instructions" {
-  value = module.gitlab_gke.root_password_instructions
+  value = module.gitlab.root_password_instructions
 }
