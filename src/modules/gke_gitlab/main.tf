4ac3131602ad2257330ab1b23120190d75d04612
asegni <segniaymen1@gmail.com>
2020-02-10 23:17:04 +0100

    Update path file

--- src/modules/gke_gitlab/main.tf
+++ src/modules/gke_gitlab/main.tf
@@ -71,7 +71,7 @@ resource "google_compute_address" "gitlab" {
 
 // Database
 resource "google_compute_global_address" "gitlab_sql" {
-  provider      = "google-beta"
+  provider      = google-beta
   project       = var.project_id
   name          = "gitlab-sql"
   purpose       = "VPC_PEERING"
@@ -81,7 +81,7 @@ resource "google_compute_global_address" "gitlab_sql" {
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  provider                = "google-beta"
+  provider                = google-beta
   network                 = google_compute_network.gitlab.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.gitlab_sql.name]
